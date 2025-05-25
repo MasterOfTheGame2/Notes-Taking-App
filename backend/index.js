7c3feb97b359fcee79d436804467308c75eaae82
Hurie Georges Pierre <hurielgpierre@outlook.com>
2025-05-25 18:43:29 -0400

    index.js

--- backend/index.js
+++ backend/index.js
@@ -12,6 +12,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", auth);
 app.use("/api/note", noteRouter);
+app.use(express.static("frontend/dist"));
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
